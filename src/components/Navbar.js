cae0a7b4ee18546b7530241148e9a3f5cf0f7c03
Shible Noman Aabir <shible.noman@sage.com>
2022-03-21 21:34:42 +0800

    Project and Folder structure created.

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import react from "react"
+
+export default function Navbar(){
+    return (
+        <div>
+            <div className="navbar-bg">
+                <img src="../assets/img/logo.png" className="logo" alt="logo" />
+                <h2>Noman's Travel Journal</h2>
+            </div>
+        </div>
+    )
+}
