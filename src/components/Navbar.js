f29863c27737f6c6daf505b2e3891f0e801d354b
Shible Chowdhury <Shible.Chowdhury@theaccessgroup.com>
2022-03-27 14:58:42 +0800

    Removed react import. Style Added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
-import react from "react"
-
 export default function Navbar(){
     return (
         <div>
-            <div className="navbar-bg">
+            <div className="navbar">
                 <img src="../assets/img/logo.png" className="logo" alt="logo" />
                 <h2>Noman's Travel Journal</h2>
             </div>
