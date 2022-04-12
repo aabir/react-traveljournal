5665da3f5e5c0c53a1ee4b3553ab84a73ca7ae11
Shible Chowdhury <Shible.Chowdhury@theaccessgroup.com>
2022-04-13 00:47:12 +0800

    Basic CSS design.

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,18 +1,18 @@
 export default function Journal(props){
     return (
         <div>
-            <div className="feature-img">
-                <img src={props.item.imageUrl} />
-            </div>
-            <div className="title-header">
-                <div className="location">{props.item.location}</div>
-                <a href={props.item.googleMapsUrl} className="location-">View on Google View</a>
-                <h2 className="main-header">{props.item.title}</h2>
-
-                <div className="journal-date">{`${props.item.startDate} - ${props.item.endDate}`}</div>
-
-                <div className="journal-body">
-                    {props.item.description}
+            <div className="wrapper">
+                <div className="feature-img">
+                    <img src={props.item.imageUrl} />
+                </div>
+                <div className="content-wrapper">
+                    <div className="location">{props.item.location}</div>
+                    <a href={props.item.googleMapsUrl} className="map-link" target={'_blank'}>View on Google View</a>
+                    <h2 className="main-header">{props.item.title}</h2>
+                    <div className="journal-date">{`${props.item.startDate} - ${props.item.endDate}`}</div>
+                    <div className="journal-body">
+                        {props.item.description}
+                    </div>
                 </div>
             </div>
         </div>
